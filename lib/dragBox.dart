ffce565348ffda95dee1755574d4078c4221dff6
dilaraycam <dilaray_cam@hotmail.com>
2021-04-22 21:07:17 +0300

    the areas where the icons will be dragged are determined

--- lib/dragBox.dart
+++ lib/dragBox.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
 
 class DragBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomRight,
-      child: Container(width: 400, height: 100, color: Colors.green),
+      child: Container(
+          width: 400,
+          height: 100,
+          color: Colors.green,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            MyButton(
+              child: Text(
+                '  1  ',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            MyButton(
+              child: Text(
+                '  2  ',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            MyButton(
+                child: Text(
+              '  3  ',
+              textDirection: TextDirection.ltr,
+            ))
+          ])),
     );
   }
 }
